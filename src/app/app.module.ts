e95c723870303468d4eed52b39866fc3a0065be6
drewawright <drewawright@gmail.com>
2019-07-29 10:14:59 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PokemonIndexComponent } from './components/pokemon/pokemon-index/pokemon-index.component';
+import { PokemonDetailComponent } from './components/pokemon/pokemon-detail/pokemon-detail.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonIndexComponent,
+    PokemonDetailComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
