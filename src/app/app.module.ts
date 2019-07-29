600a153f9745ba544fcabcb22e83d49d236e2e43
jdorhauer <jdorhauer@yahoo.com>
2019-07-29 13:33:17 -0400

    accepting base changes to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MatGridListModule } from '@angular/material';
 import { PokemonIndexComponent } from './components/pokemon/pokemon-index/pokemon-index.component';
 import { PokemonDetailComponent } from './components/pokemon/pokemon-detail/pokemon-detail.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -20,7 +21,8 @@ import { AboutComponent } from './components/about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
